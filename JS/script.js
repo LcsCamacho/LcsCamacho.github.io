dd76fc031253d0d3908ba378c097b3c3b6c5f3aa
hortenciojunior <you@example.com>
2022-09-23 08:48:18 -0300

    att portifolio

--- JS/script.js
+++ JS/script.js
@@ -10,23 +10,23 @@ let vol4 = 0;
 
 setInterval(() => {
 liq.style.height = vol + "%";
-    vol += 4;
-    if (vol >= 96) { vol = 0 }
+    vol += 8;
+    if (vol >= 90) { vol = 90 }
 }, 500)
 setInterval(() => {
     liq2.style.height = vol2 + "%";
     vol2 += 4;
-    if (vol2 >= 20) { vol2 = 0 }
+    if (vol2 >= 20) { vol2 = 20 }
 }, 500)
 setInterval(() => {
     liq3.style.height = vol3 + "%";
     vol3 += 4;
-    if (vol3 >= 40) { vol3 = 0 }
+    if (vol3 >= 40) { vol3 = 39 }
 }, 500)
 setInterval(() => {
     liq4.style.height = vol4 + "%";
-    vol4 += 4;
-    if (vol4 >= 96) { vol4 = 0 }
+    vol4 += 8;
+    if (vol4 >= 90) { vol4 = 89 }
 }, 500)
 
 let myaudio = document.getElementById("welcome-audio")
